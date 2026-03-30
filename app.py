fdbe5ec79bd222b2e87d24f00f4668e1fc8208bf
abhishekrajchoudhary101 <abhishekrajchoudhary101@gmail.com>
2026-03-31 02:13:33 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+# Load model and scaler
+model = joblib.load("model.pkl")
+scaler = joblib.load("scaler.pkl")
+
+@app.route('/')
+def home():
+    return "XGBoost API Running"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = request.json
+
+        # get input
+        soil_moisture = float(data['soil_moisture'])
+        temperature = float(data['temperature'])
+        humidity = float(data['humidity'])
+
+        # IMPORTANT: same order as training
+        features = np.array([[soil_moisture, temperature, humidity]])
+
+        # scale
+        features = scaler.transform(features)
+
+        # predict
+        prediction = model.predict(features)
+
+        return jsonify({
+            "pump": int(prediction[0])
+        })
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
